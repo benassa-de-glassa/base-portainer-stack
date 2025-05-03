fca3b6f86d0a869845f3019298b3fb7e791a481f
Benedikt Schmidhuber <schmidhuber.benedikt@gmail.com>
2025-05-03 10:30:24 +0200

    try this

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,6 @@ const PORT = 3000;
 app.use(express.json());
 setRoutes(app);
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, "::", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
